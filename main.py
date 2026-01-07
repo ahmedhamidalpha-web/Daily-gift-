8683caaf718d4350fc9d1feed25e4ff4730bf921
ahmedhamidalpha-web <ahmedhamidalpha@gmail.com>
2026-01-07 08:42:55 +0200

    main.py

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ from telethon import TelegramClient
 import asyncio
 import os
 
-# قراءة البيانات من Environment Variables
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
+phone = os.environ["PHONE_NUMBER"]
 
 bot_username = os.environ["BOT_USERNAME"]  # بدون @
 message = "🎁 الهدية اليومية"
 
 async def main():
-    # إنشاء الجلسة وتشغيل العميل
     client = TelegramClient("session", api_id, api_hash)
-    await client.start()
 
-    # حلقة لا نهائية لإرسال الرسالة كل 30 دقيقة
+    await client.start(phone=phone)
+
     while True:
         await client.send_message(bot_username, message)
-        await asyncio.sleep(1800)  # 1800 ثانية = 30 دقيقة
+        await asyncio.sleep(1800)  # 30 دقيقة
 
-# تشغيل البرنامج
 asyncio.run(main())
